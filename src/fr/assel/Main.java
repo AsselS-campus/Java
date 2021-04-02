2bf78ff7c3e2edf61d9851a84ac2802e4b5b1690
Assel.Sultangazieva <assel.sultangazieva@le-campus-numerique.fr>
2021-04-02 16:50:50 +0200

    BDD Java

--- src/fr/assel/Main.java
+++ src/fr/assel/Main.java
@@ -1,16 +1,21 @@
 package fr.assel;
 
 
+import fr.assel.characters.Ennemi;
+
 import java.util.Scanner;
 
 public class Main {
     /*Main méthode*/
     public static void main(String[] args) {
+        BDD.dbConnect();
+        BDD bdd=new BDD();
+        bdd.saveCharacter();
 
-        /*déclaration et création de l'objet*/
-        /*j'instancie l'objet menu puis j'appelle la méthode nouveauPersonnage dessus*/
         Menu menu = new Menu();
         menu.display();
+        /*déclaration et création de l'objet*/
+        /*j'instancie l'objet menu puis j'appelle la méthode nouveauPersonnage dessus*/
     }
  }
 
